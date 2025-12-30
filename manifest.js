6cccb9dd1dc83bd434f1a3d6fc0618afdb31d1f9
MD MONGERUL HAQUE <andrewlaraujo70@gmail.com>
2025-12-30 22:03:47 +0600

    Create manifest.js

---
+++ manifest.js
@@ -0,0 +1,21 @@
+{
+  "name": "MyGP Blog",
+  "short_name": "GP Blog",
+  "start_url": ".",
+  "display": "standalone",
+  "background_color": "#ffffff",
+  "theme_color": "#10B981",
+  "orientation": "portrait",
+  "icons": [
+    {
+      "src": "https://cdn-icons-png.flaticon.com/512/3523/3523063.png",
+      "sizes": "192x192",
+      "type": "image/png"
+    },
+    {
+      "src": "https://cdn-icons-png.flaticon.com/512/3523/3523063.png",
+      "sizes": "512x512",
+      "type": "image/png"
+    }
+  ]
+}
